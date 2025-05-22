a9c08709b909a4893ff14c000867e447466d870e
amirValiulla32 <amirvaliulla32@gmail.com>
2025-05-21 18:39:37 -0700

    added lambda wrapper for API testing (aws)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.news import router as news_router
-print(" Importing news_router...")
+from mangum import Mangum
 app = FastAPI()
 app.include_router(news_router)
-print(" news_router included!")  
+
 @app.get("/")
 def read_root():
     return {"message": "Boycat News Microservice is running!"}
+
+handler = Mangum(app)
--- routes/news.py
+++ routes/news.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, Query
 from typing import List
-
+import json
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from rss_parser.collector import fetch_latest
 from rss_parser.filter import keyword_filter
+from fastapi.responses import Response
+
 
 router = APIRouter(prefix="/news")
 
@@ -21,16 +25,20 @@ def get_news(
     # 2. fetch & filter
     raw_articles = fetch_latest(limit=50)          # pull more than we need
     articles = keyword_filter(raw_articles, keywords, max_results=limit)
-    ##articles = raw_articles[:limit]
 
     # 3. shape response
-    return {
+    response_data = {
         "campaign": campaign,
         "brand": brand,
         "keywords": keywords,
         "count": len(articles),
         "articles": articles,
     }
+    
+    return Response(
+        content=json.dumps(jsonable_encoder(response_data), indent=2),
+        media_type="application/json"
+    )
 @router.get("/debug")
 def debug_rss():
     from rss_parser.collector import fetch_latest
