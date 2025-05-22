6a6a38e582d3aa016fcd89e86ac723d769cc1abe
amirValiulla32 <amirvaliulla32@gmail.com>
2025-05-22 10:04:06 -0700

    remove lambda handler, switching to railway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from mangum import Mangum
+##from mangum import Mangum
 from routes.news import router as news_router
 
 app = FastAPI()
@@ -10,4 +10,4 @@ def read_root():
     return {"message": "Boycat News Microservice is running!"}
 
 # Only for AWS Lambda — ignored locally
-handler = Mangum(app)
+##handler = Mangum(app)
