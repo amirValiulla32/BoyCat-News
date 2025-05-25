b44c356c5bcc113dc797c83a387ff4c92b6b263f
amirValiulla32 <amirvaliulla32@gmail.com>
2025-05-25 16:25:15 -0700

    security fix v1.0

--- auth.py
+++ auth.py
@@ -8,5 +8,5 @@ api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 def verify_api_key(api_key: str = Security(api_key_header)):
     print(f"Received API key: {api_key}")
-    if api_key != API_KEY:
+    if not api_key or api_key != API_KEY:
         raise HTTPException(status_code=403, detail="Invalid API Key")
